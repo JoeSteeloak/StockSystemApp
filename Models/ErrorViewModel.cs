e89ac4cffc7094025061cbd6155d59847e13299f
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-03-11 09:48:32 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StockSystemApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
